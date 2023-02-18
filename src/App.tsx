efa3ea806dd922d4af1672ca61446e2f675715a0
Vinicius <vinicezar800@hotmail.com>
2023-02-18 11:52:00 -0300

    feat: finished home page styles and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,15 @@
-import { MdContentCopy } from "react-icons/md";
-import { IoReloadCircleOutline } from "react-icons/io5";
-import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 import "./index.scss";
 
 export default function App() {
   return (
-    <div className="p-2">
-      <div className="bg-secondary mb-5 p-4 w-25"></div>
-
-      <main className="border">
-        <div className="row">
-          <div className="col-12">
-            <div className="d-flex justify-content-center p-4">
-              <div className="display-generated-email">
-                <div className="display-group">
-                  <span className="mx-2 d-block">
-                    Your temporary email address
-                  </span>
-                  <div className="d-flex align-items-center input-group">
-                    <input
-                      type="text"
-                      className="form-control fw-bold text-secondary"
-                      value={"dysyky@ema-sofia.eu"}
-                      disabled
-                    />
-                    <button className="btn btn-secondary fw-semibold">
-                      <MdContentCopy className="fs-4" /> Copy
-                    </button>
-                  </div>
-                </div>
-                <div className="d-flex justify-content-center align-items-center">
-                  <div className="d-flex align-items-center py-3">
-                    <div className="d-flex align-items-center fw-semibold text-secondary">
-                      Autorefresh in{" "}
-                      <div
-                        className="d-block"
-                        style={{ width: 30, fontSize: ".75rem", marginLeft: 5 }}
-                      >
-                        <CircularProgressbarWithChildren value={50}>
-                          <span className="fw-bold">5</span>
-                        </CircularProgressbarWithChildren>
-                      </div>
-                    </div>
-                    <button className="btn btn-transparent text-secondary fw-semibold d-flex align-items-center p-2 mx-3">
-                      <IoReloadCircleOutline className="fs-2" /> Refresh
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-6"></div>
-          <div className="col-6"></div>
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,84 @@
+import { MdContentCopy } from "react-icons/md";
+import { IoReloadCircleOutline } from "react-icons/io5";
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+
+export default function Home() {
+  return (
+    <div className="wrapper-page vh-100 p-2">
+      <div className="grid-area-brand">
+        <div className="bg-secondary p-4 w-25 rounded"></div>
+      </div>
+      <div className="grid-area-email border rounded">
+        <div className="grid-area-generated-email border-bottom">
+          <div className="d-flex justify-content-center align-items-center flex-column">
+            <div className="generated-email p-4">
+              <div className="generated-email-group">
+                <span className="d-block ms-2 fw-semibold">
+                  Your temporary email address
+                </span>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control fw-bold text-secondary"
+                    defaultValue={"dysyky@ema-sofia.eu"}
+                  />
+                  <button className="btn btn-secondary fw-semibold">
+                    <MdContentCopy className="fs-5" /> Copy
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center p-2">
+              <div className="d-flex align-items-center fw-semibold text-secondary mx-3">
+                Autorefresh in{" "}
+                <div className="circular-refresh-progress">
+                  <CircularProgressbarWithChildren value={60}>
+                    5
+                  </CircularProgressbarWithChildren>
+                </div>
+              </div>
+              <button className="btn btn-transparent text-secondary d-flex align-items-center fw-semibold">
+                <IoReloadCircleOutline className="fs-3" /> Refresh
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="grid-area-emails border-end">
+          <div className="area-divisor border-bottom d-flex align-items-center py-2 px-3">
+            <h4 className="m-0">Inbox</h4>
+          </div>
+          <ul className="list-group">
+            <li className="list-group-item rounded-0 border-0 border-bottom px-1">
+              <h5 className="fw-bold m-0">Hello</h5>
+              <h6 className="fw-bold text-primary m-0">Welcome</h6>
+              <p className="text-secondary m-0">
+                Your temp e-mail address is ready...
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="grid-area-content">
+          <div className="area-divisor bg-secondary border-bottom"></div>
+
+          <div className="px-2 bg-secondary email-content">
+            <h4 className="fw-bold py-2 mb-0 mx-4">Welcome</h4>
+            <div className="bg-white p-2 rounded border">
+              <p>Hi dfr,</p>
+              <p className="fw-bold m-0">
+                <span className="bg-secondary">
+                  Your temp e-mail address is ready
+                </span>
+              </p>
+              <p>
+                If you need help read the information below and do not hesitate
+                to contact us.
+              </p>
+              <p className="m-0">All the best,</p>
+              <p>DropMail</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
